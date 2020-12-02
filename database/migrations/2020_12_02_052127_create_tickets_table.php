656d4795dfb9986faf4a279fc9c76f3eb83f7d15
Mohamed Arushadh <attu@Attus-MacBook-Pro.local>
2020-12-02 10:29:16 +0500

    tickets migration

---
+++ database/migrations/2020_12_02_052127_create_tickets_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table-hn
+            $table->integer('chn_mail');
+            $table->integer('chn_livechat');
+            $table->integer('chn_twitter');
+            $table->integer('chn_facebook');
+            $table->integer('chn_whatsapp');
+            $table->integer('chn_viber');
+            $table->integer('chn_telegram');
+            $table->integer('chn_messenger');
+            $table->integer('esc_med_br_open');
+            $table->integer('esc_med_jc_open');
+            $table->integer('esc_med_er_open');
+            $table->integer('esc_med_br_closed');
+            $table->integer('esc_med_jc_closed');
+            $table->integer('esc_med_er_closed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
